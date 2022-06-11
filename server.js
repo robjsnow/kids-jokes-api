7e4f3177386a65675003ac130dfbb540b8b6e606
Robert Snow <robjsnow@gmail.com>
2022-06-10 22:26:19 -0500

    added cors to server.js

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ const mongoose = require("mongoose");
 const Joke = require("./models/Joke");
 const userName = "joker";
 const password = "B7w8tHEMNKL985BW";
+const cors = require("cors");
 
 //Database uri
 const dbURI = `mongodb+srv://${userName}:${password}@cluster0.a8umj.mongodb.net/JokesDB?retryWrites=true&w=majority`;
 //Server port
 const port = process.env.PORT || 3000;
+app.use(cors());
 
 async function getRndInteger(min, max) {
   try {
