8dc75384a0eba5787fa693948dd600d2c90e9e90
Carmen Smith <cheylaracarm77@gmail.com>
2022-06-12 22:09:50 -0500

    added 401 jokes to server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const numberOfJokes = 300;
+const numberOfJokes = 401;
 const mongoose = require("mongoose");
 const Joke = require("./models/Joke");
 const userName = "joker";
